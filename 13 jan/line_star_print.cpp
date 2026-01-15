42d13d17e18feca8ac52e9ce340502185b8a84bd
ankit1141r <ak7047053707@gmail.com>
2026-01-15 13:45:12 +0530

    star1

---
+++ 13 jan/line_star_print.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int number;
+    cout << "n:";
+   cin >> number;
+    for (int i =number; i>=1; i--) {
+        cout << "*";
+    }
+    return 0;
+}
