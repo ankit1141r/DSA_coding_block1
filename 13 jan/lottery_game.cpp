5815d6dba55649652a7bfd56701da3755d19f8f2
ankit1141r <ak7047053707@gmail.com>
2026-01-16 16:06:23 +0530

    lottary1

---
+++ 13 jan/lottery_game.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int n;
+ cin>>n;
+ if(n>=1100 && n<=1500){
+     cout<<"Cycle";}
+    else if(n>=300 && n<=460){
+        cout<<"Macbook";}
+         else if(n>=200 && n<=280){
+        cout<<"Kurkure";}
+         else if(n>50 && n<=80){
+        cout<<"Bike";}
+    else{
+        cout<<"Better luck next time";}
+
+    return 0;
+}
