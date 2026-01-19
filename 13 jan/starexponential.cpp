47449d650fee2551f717934323b8e27c31e6db44
ankit1141r <ak7047053707@gmail.com>
2026-01-19 17:41:59 +0530

    starexponential

---
+++ 13 jan/starexponential.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << "  ";
+        }
+        for (int k = 1; k <=i; k++) {
+            if(k%2==0){
+                cout<<"! ";
+            }
+            else{
+            cout << "* ";
+            }
+        }  
+         for (int k = 2; k <=i; k++) {
+            if(i%2!=0){
+             if(k%2==0){
+                cout<<"! ";
+            }
+            else{
+            cout << "* ";
+            }
+        }
+        else{
+            if(k%2==0){
+                cout<<"* ";
+            }
+            else{
+            cout << "! ";
+            }
+        }
+    }
+      
+        cout << endl;
+    }
+    return 0;
+}
