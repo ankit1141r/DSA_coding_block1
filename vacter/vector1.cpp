a0ee40b527eeb9b0e0226f7a229a021f6c066ba8
ankit1141r <ak7047053707@gmail.com>
2026-02-10 14:45:52 +0530

    vector

--- vacter/vector1.cpp
+++ vacter/vector1.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 int main() {
 vector<int> a;
-  a.push_back(1);
+  a.push_back(4);
   a.push_back(2);
   a.push_back(3);
-  a.push_back(4);
+  a.push_back(1);
   a.push_back(5);
   a.pop_back();
 
@@ -16,5 +17,35 @@ vector<int> a;
       cout<<endl;
       cout<<a.front()<<endl;
       cout<<a.back()<<endl;
+      reverse(a.begin(),a.end());
+     
+      for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+      } 
+      cout<<endl;
+      sort(a.begin(),a.end());
+      for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+      }
+    cout<<endl; 
+      a.insert(a.begin()+2,6);
+      for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+      }
+      cout<<endl;
+      a.insert(a.end()-2,6);
+      for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+      }
+      cout<<endl;
+      int n=5;
+      vector<int>arr(n,1);
+      for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+      }
+
+      cout<<endl;
+      cout<<a.size()<<endl;
+      cout<<a.capacity()<<endl;
     return 0;
 }
