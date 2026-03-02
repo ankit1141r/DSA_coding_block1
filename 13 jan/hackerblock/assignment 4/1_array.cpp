b87730528f90d4177058b8e137c4f68ba89394c2
ankit1141r <ak7047053707@gmail.com>
2026-03-02 21:34:14 +0530

    assignment

---
+++ 13 jan/hackerblock/assignment 4/1_array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+	int arr[n],brr[n],j=0;
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	for(int i=0;i<n;i++){
+		brr[arr[i]]=i;
+	}
+	for(int i=0;i<n;i++){
+		cout<<brr[i]<<" ";
+	}
+	return 0;
+}
