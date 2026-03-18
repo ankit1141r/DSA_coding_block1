cba16a70ded1a2d2480b1127351edfea784a4064
ankit1141r <ak7047053707@gmail.com>
2026-03-18 21:52:35 +0530

    update

--- 13 jan/hackerblock/assignment 5/18_2d_array.cpp
+++ 13 jan/hackerblock/assignment 5/18_2d_array.cpp
@@ -13,11 +13,15 @@ int main() {
    for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(arr[i][j]==1){
-                for(int k=0;k<n;k++){
-              arr[i][k] =1;
+                for(int k=0;k<m;k++){
+              if(arr[k][j]==0){
+               arr[k][j]=-1;
             }
-            for(int k=0;k<m;k++){
-              arr[k][j] =1;
+            }
+            for(int k=0;k<n;k++){
+              if(arr[i][k]==0){
+               arr[i][k]=-1;
+            }
             }
 
         }
@@ -25,8 +29,8 @@ int main() {
     }
       for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            if(arr[i][j]==1){
-               arr[i][j]==1;
+            if(arr[i][j]==-1){
+               arr[i][j]=1;
             }
         }
         }  
