ebce9f0b52404e979599a8960e902a9ce2ed7e49
ankit1141r <ak7047053707@gmail.com>
2026-03-22 21:22:01 +0530

    assignment

---
+++ 13 jan/hackerblock/assignment 6/15_string.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+bool ispalindrome(string s){
+	int m=s.length();
+int right=m-1;
+for(int i=0;i<(m+1)/2;i++){
+	if(s[i]!=s[right]){
+		return false;
+		break;
+	}
+	right--;
+}
+return true;
+}
+int main() {
+	string s;
+	cin>>s;
+	int size=1,count=0;
+	for(int j=0;j<s.length();j++){
+	for(int i=0;i<s.length()-j;i++){
+		string str=	s.substr(i,size);
+		if (ispalindrome(str)){
+			count++;
+		}
+	}
+	size++;
+	}
+ cout<<count;
+	return 0;
+}
